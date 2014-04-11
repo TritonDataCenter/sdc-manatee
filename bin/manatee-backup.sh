d3a8f830e14e6dd0d775453733f68f9772709370
Yunong Xiao <yunong@joyent.com>
2014-04-11 22:50:44 +0000

    TOOLS-393 add manatee-backup script

---
+++ bin/manatee-backup.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+#
+# manatee-backup: backup the current manatee instnace
+#
+
+if [[ -n "$TRACE" ]]; then
+    export PS4='[\D{%FT%TZ}] ${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+    set -o xtrace
+fi
+set -o errexit
+set -o pipefail
+
+function usage() {
+cat << HERE
+usage: $0
+
+Backup this manatee instance
+
+OPTIONS:
+-h Show this message
+-v Verbose
+
+EXAMPLE:
+$0 sdc
+HERE
+}
+
+while getopts "h:v" OPTION
+do
+    case $OPTION in
+        h)
+            usage
+            exit 1
+            ;;
+        v)
+            verbose=true
+            set -o xtrace
+            ;;
+        ?)
+            usage
+            exit
+            ;;
+    esac
+done
+
+if [[ -z $verbose ]]
+then
+    BACKUP_FILE=$1
+else
+    BACKUP_FILE=$2
+fi
+
+if [[ -z $BACKUP_FILE ]]
+then
+    echo 'no backup file specified'
+    exit 1
+fi
+
+# get the latest zfs snapshot
+SNAPSHOT=$(zfs list -o name -t snapshot | grep manatee | tail -1)
+if [[ -z $SNAPSHOT ]]
+then
+    echo 'no manatee snapshots found'
+    exit 1
+fi
+
+zfs send $SNAPSHOT > $BACKUP_FILE
