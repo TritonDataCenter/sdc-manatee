5cd7d80d7a5c737b450374e52bde4ce43656f4a7
Yunong Xiao <yunong@joyent.com>
2014-04-15 18:16:57 +0000

    MANATEE-167 use manatee tools from client

--- boot/setup.sh
+++ boot/setup.sh
@@ -119,9 +119,9 @@ function add_manatee_profile_functions {
     echo "zbunyan() { bunyan -c \"this.component !== 'ZKPlus'\"; }" >> $PROFILE
     echo "mbunyan() { bunyan -c \"this.component !== 'ZKPlus'\"  -c 'level >= 30'; }" >> $PROFILE
     echo "manatee-history(){ /opt/smartdc/manatee/node_modules/manatee/bin/manatee-history '$SHARD' \"\$ZK_IPS\"; }" >> $PROFILE
-    echo "manatee-stat() { /opt/smartdc/manatee/node_modules/manatee/bin/manatee-stat -p \"\$ZK_IPS\"; }" >> $PROFILE
-    echo "manatee-clear(){ /opt/smartdc/manatee/node_modules/manatee/bin/manatee-clear '$SHARD' '$ZONE_IP' \"\$ZK_IPS\"; }" >> $PROFILE
-    echo "manatee-snapshots(){ /opt/smartdc/manatee/node_modules/manatee/bin/manatee-snapshots '$DATASET'; }" >> $PROFILE
+    echo "manatee-stat() { /opt/smartdc/manatee/node_modules/.bin/manatee-stat -p \"\$ZK_IPS\"; }" >> $PROFILE
+    echo "manatee-clear(){ /opt/smartdc/manatee/node_modules/.bin/manatee-clear '$SHARD' '$ZONE_IP' \"\$ZK_IPS\"; }" >> $PROFILE
+    echo "manatee-snapshots(){ /opt/smartdc/manatee/node_modules/.bin/manatee-snapshots '$DATASET'; }" >> $PROFILE
     echo "msitter(){ tail -f \`svcs -L manatee-sitter\` | mbunyan; }" >> $PROFILE
     echo "mbackupserver(){ tail -f \`svcs -L manatee-backupserver\` | mbunyan; }" >> $PROFILE
     echo "msnapshotter(){ tail -f \`svcs -L manatee-snapshotter\` | mbunyan; }" >> $PROFILE
