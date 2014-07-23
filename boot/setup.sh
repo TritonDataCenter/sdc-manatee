0579df949695becd8cadae6508a7238cd487cc4b
Yunong Xiao <yunong@joyent.com>
2014-07-23 18:56:06 +0000

    update node-manatee dep

--- boot/setup.sh
+++ boot/setup.sh
@@ -135,7 +135,7 @@ function add_manatee_profile_functions {
     echo "mbunyan() { bunyan -c \"this.component !== 'ZKPlus'\"  -c 'level >= 30'; }" >> $PROFILE
     echo "manatee-history(){ /opt/smartdc/manatee/node_modules/node-manatee/bin/manatee-history 'sdc' \"\$ZK_IPS\"; }" >> $PROFILE
     echo "manatee-stat() { /opt/smartdc/manatee/node_modules/.bin/manatee-stat -p \"\$ZK_IPS\"; }" >> $PROFILE
-    echo "manatee-clear(){ /opt/smartdc/manatee/node_modules/.bin/manatee-clear 'sdc' '$ZONE_IP' \"\$ZK_IPS\"; }" >> $PROFILE
+    echo "manatee-clear(){ /opt/smartdc/manatee/node_modules/.bin/manatee-clear 'sdc' \"\$ZK_IPS\"; }" >> $PROFILE
     echo "manatee-snapshots(){ /opt/smartdc/manatee/node_modules/.bin/manatee-snapshots '$DATASET'; }" >> $PROFILE
     echo "msitter(){ tail -f \`svcs -L manatee-sitter\` | mbunyan; }" >> $PROFILE
     echo "mbackupserver(){ tail -f \`svcs -L manatee-backupserver\` | mbunyan; }" >> $PROFILE
