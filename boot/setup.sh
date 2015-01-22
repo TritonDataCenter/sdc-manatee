0972a5d02f11a4f805f766ef15a89aadd1415d8d
Matt Smillie <matt.smillie@joyent.com>
2015-01-22 15:05:10 -0800

    MANATEE-243 remove hardcoded binder address from setup

--- boot/setup.sh
+++ boot/setup.sh
@@ -39,7 +39,6 @@ function sdc_manatee_setup {
     PG_LOG_DIR=/var/pg
     ZONE_IP=$(json -f /var/tmp/metadata.json ADMIN_IP)
     SHARD=$(json -f /var/tmp/metadata.json SERVICE_NAME)
-    BINDER_ADMIN_IPS=$(json -f /var/tmp/metadata.json binder_admin_ips)
 
     # Cookie to identify this as a SmartDC zone and its role
     mkdir -p /var/smartdc/$role
@@ -131,8 +130,6 @@ function common_manatee_setup {
 }
 
 function add_manatee_profile_functions {
-    ZK_IPS=${BINDER_ADMIN_IPS}
-
     #.bashrc
     echo "export PATH=\$PATH:/opt/smartdc/manatee/bin/:/opt/smartdc/manatee/pg_dump/:/opt/smartdc/manatee/node_modules/manatee/bin" >>/root/.bashrc
     echo "export MANPATH=\$MANPATH:/opt/smartdc/manatee/node_modules/manatee/man" >> /root/.bashrc
