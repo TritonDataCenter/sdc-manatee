9388f0b14a9eb89d6e93785e0f7e6f3aa851e868
Brian Bennett <bahamat@digitalelf.net>
2015-12-28 12:46:50 -0800

    MANATEE-298 manatee-stat-alarm.sh always returns fail for ONMW

--- bin/manatee-stat-alarm.sh
+++ bin/manatee-stat-alarm.sh
@@ -15,21 +15,6 @@ export HOME=/root
 export PATH=$PATH:/opt/local/bin
 source /root/.bashrc
 
-shard=$(cat /opt/smartdc/manatee/etc/sitter.json | json shardPath | cut -d '/' -f3)
-topology=$(manatee-stat| json -D / $shard)
-primary=
-sync=
-
-if [[ -z $topology ]]; then
+if ! manatee-adm verify; then
     echo fail && exit;
-fi
-
-primary=$(echo $topology | json primary.repl.sync_state)
-sync=$(echo $topology | json sync.repl.sync_state)
-
-if [[ $primary != 'sync' ]]; then
-    echo fail && exit;
-fi
-if [[ $sync != 'async' ]]; then
-    echo fail;
 fi
